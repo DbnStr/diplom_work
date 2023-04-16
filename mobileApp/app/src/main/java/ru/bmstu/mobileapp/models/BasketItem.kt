fcf4c690ed89cd27baecb2ee8047e16fe04f23d5
DBNSTR <alexpich2002@yandex.ru>
2023-04-16 14:31:17 +0300

    Update BasketItem.kt

--- mobileApp/app/src/main/java/ru/bmstu/mobileapp/models/BasketItem.kt
+++ mobileApp/app/src/main/java/ru/bmstu/mobileapp/models/BasketItem.kt
@@ -3,6 +3,6 @@ package ru.bmstu.mobileapp.models
 data class BasketItem(
     var name: String? = null,
     var quantity: String? = null,
-    var one_item_cost: String? = null,
+    var oneItemCost: String? = null,
     var amount: String? = null
 )
